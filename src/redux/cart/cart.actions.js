5a6577ecd692e12cd3fe77c2126c816531411bdf
Valer Muresan <muresanvaler86@yahoo.com>
2022-01-09 22:17:40 +0200

    add increase decrease functionality of cartItem from checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -9,3 +9,12 @@ export const addItem = item => ({
     payload: item
 });
 
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVES_ITEM,
+    payload:item
+});
+
+export const clearItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+})
